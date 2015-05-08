a602bdacb345e7fcdc1b42878bc5f48649d6a729
Hank Bao <hankbao84@gmail.com>
2015-05-08 20:32:04 +0800

    add missing files

---
+++ KVOController/KVOController.h
@@ -0,0 +1,20 @@
+//
+//  KVOController.h
+//  KVOController
+//
+//  Created by Hank Bao on 15/5/8.
+//  Copyright (c) 2015年 Kimon Tsinteris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVOController.
+FOUNDATION_EXPORT double KVOControllerVersionNumber;
+
+//! Project version string for KVOController.
+FOUNDATION_EXPORT const unsigned char KVOControllerVersionString[];
+
+#import <KVOController/FBKVOController.h>
+#import <KVOController/NSObject+FBKVOController.h>
+
+
